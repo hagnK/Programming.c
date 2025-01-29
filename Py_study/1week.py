4c43f82b6734d2de1cc6dff6cfe1c2cc78e5257d
hagnK <akddy3@gmail.com>
2025-01-29 19:43:12 +0900

    commit

--- Py_study/1week.py
+++ Py_study/1week.py
@@ -1,2 +1,85 @@
-print("Hellow python")
-10 +20
+''' 실습 1
+print("# 하나만 출력합니다")
+print("Hello Python Programmin")
+print()
+
+print("# 여러 개를 출력합니다")
+print(10, 20, 30, 40, 50)
+print("안녕하세요", "저의", "이름은", "구현규입니다")
+print()
+
+print("# 아무것도 출력하지 않습니다")
+print("-- 확인 구현규 --")
+print()
+print()
+print("-- 확인 구현규 --")'''
+
+'''실습 2
+print("이름\t나이\t지역")
+print("윤인성\t25\t강서구")
+print("윤아린\t24\t강서구")
+print("구름\t3\t강서구")'''
+
+'''실습 3
+print("안녕하세요"[-1])
+print("안녕하세요"[-2])
+print("안녕하세요"[-3])
+print("안녕하세요"[-4])
+print("안녕하세요"[-5])'''
+
+"""실습 4
+pi = 3.14159265
+r = 10
+
+print("원주율 = ",pi)
+print("반지름 = ",r)
+print("원의 둘레 ",2*pi*r)
+print("원의 넓이 ",pi*r*r)"""
+
+'''실습 5
+string = input("입력 > ")
+print("자료 ",string)
+print("타입 ",type(string))'''
+
+'''실습 6
+str = input("입력 > ")
+print("입력 + 100", str + 100)'''
+
+'''실습 7
+strign_a = input("입력> ")
+int_a = int(strign_a)
+
+strign_b = input("입력> ")
+int_b = int(strign_b)
+
+print("문자열의 합 ",strign_a+strign_b)
+print("정수의 합 ",int_a+int_b) '''
+
+'''실습 8
+int_a = int("52")
+string_a = float("52.273")
+
+print(type(int_a), int_a)
+print(type(string_a), string_a)'''
+
+'''실습 9
+string_a = float(input("첫 번째 숫자> "))
+string_b = float(input("두 번째 숫자> "))
+
+print("덧셈 결과> ",string_a+string_b)
+print("뺄셈 결과> ",string_a-string_b)
+print("곱셈 결과> ",string_a*string_b)
+print("나눗눗셈 결과> ",string_a/string_b)'''
+
+'''실습 10
+output_a = str(52)
+output_b = str(52.273)
+
+print(type(output_a), output_a)
+print(type(output_b), output_b)'''
+
+'''실습 11
+inch = int(input("inch 단위의 숫자를 입력해주세요: "))
+cm = inch*2.54
+
+print(inch,"inch는 cm 단위로", cm, "입니다")'''
